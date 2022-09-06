a4ece6c020e2203a8b525a03be936a61b5d72348
kiki <1604540911@qq.com>
2022-09-06 18:12:36 +0800

    feat:login登录页完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+// import Login from '@/views/Login' 不需要写
 Vue.use(VueRouter)
 
+// 单页面SPA: 缺点: 首屏加载速度很慢,使用路由懒加载(使用到了路由再去请求他)
 const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/Login') // 懒加载
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar title="登录" class="nav-bar" />
+    <!-- 表单 -->
+    <!-- van-field: 输入框,有text类,password类,文本域类,验证码类 -->
+    <!-- v-model: 用户输入的值 -->
+    <!-- name: 标识表单项, 一般写上 -->
+    <!-- :rules="[{ required: 必填, message: '错误信息',pattern:正则,trigger:规则的触发时机onChange,onblue(默认值) }]" -->
+    <!-- 加字体图标: 用插槽 -->
+    <van-form @submit="onSubmit" class="form">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[
+          { required: true, message: '请填写手机号' },
+          { pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/, message: '手机格式错误' }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="[
+          { required: true, message: '请填写验证码' },
+          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+//  :deep 深度选择器(发生组件穿透时起作用,或者直接去掉scoped也可以)
+.nav-bar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+}
+:deep(.form) {
+  .van-cell__title {
+    flex: 1;
+  }
+  .van-cell__value {
+    flex: 19;
+  }
+  toutiao {
+    font-size: 4px; // postcss是按照375的大小设计的,我们的代码是750,所以谷歌上是20px
+  }
+}
+</style>
